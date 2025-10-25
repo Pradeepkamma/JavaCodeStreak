72275f5de55f6aee626943eedb6ea6f9f5ce9d3a
Kamma Pradeep <142683667+Pradeepkamma@users.noreply.github.com>
2025-10-25 13:39:52 +0530

    Update SumNumRange.java

--- SumNumRange.java
+++ SumNumRange.java
@@ -1,1 +1,14 @@
+//Find the Sum of the Numbers in a Given Range in Java
+
+public class SumNumRange {
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 10;
+		int sum = 0;
+		
+		for(int i=a;i<=b;i++)
+			sum += i;
+		System.out.println("The sum is " + sum);
+	}
+}
 
