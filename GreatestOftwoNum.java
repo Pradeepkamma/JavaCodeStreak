cd4b4f727aac4e57e61728578c885f0544af7f4f
Kamma Pradeep <142683667+Pradeepkamma@users.noreply.github.com>
2025-10-25 13:41:37 +0530

    Update GreatestOftwoNum.java

--- GreatestOftwoNum.java
+++ GreatestOftwoNum.java
@@ -1,1 +1,13 @@
+//Java Program to Find Greatest of Two Numbers
+
+public class GreatestOftwoNum {
+	public static void main(String[] args) {
+		int a = 32;
+		int b = 6;
+		if(a>b)
+			System.out.println(a + " is greater");
+		else
+			System.out.println(b + " is greater");
+	}
+}
 
