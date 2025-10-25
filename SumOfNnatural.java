9af716edee63cc078b502dc0a7353360bdc52823
Kamma Pradeep <142683667+Pradeepkamma@users.noreply.github.com>
2025-10-25 13:37:30 +0530

    Update SumOfNnatural.java

--- SumOfNnatural.java
+++ SumOfNnatural.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+//Find the Sum of N Natural Numbers in Java
+//user input type
+
+public class SumOfNnatural {
+  public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter N: ");
+		int n = sc.nextInt();
+		int sum = 0;
+		
+		for (int i =1; i<=n; i++)
+			sum += i;
+		System.out.println(sum);	
+	}
+}
