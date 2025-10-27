ef6df6f74b226ac39082e6747c12c8810c5a4bb8
Kamma Pradeep <142683667+Pradeepkamma@users.noreply.github.com>
2025-10-27 14:31:43 +0530

    Update GreatestOFThree.java

--- GreatestOFThree.java
+++ GreatestOFThree.java
@@ -1,1 +1,25 @@
 
+//Find the Greatest Number Among Three Numbers in Java
+
+public class GreatestOFThree {
+
+	public static void main(String[] args) {
+		int num1 = 20, num2 = 6, num3 = 267;
+		
+		if (num1 >= num2 && num1 >= num3)
+			System.out.println (num1 + " is the greater");
+		
+		else if (num2 >= num1 && num2 >= num3)
+			System.out.println (num2 + " is the greater");
+		
+		else if (num3 >= num1 && num3 >= num2)
+		System.out.println (num3 + " is the greater");
+		//else
+		//System.out.println (num3 + " is the greater");
+
+
+
+	}
+
+}
+
